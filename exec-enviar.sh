1119d07a040d04917d094f61b3372d71621d6560
Marcos Morais <mmstec@gmail.com>
2025-07-15 13:50:06 +0000

    Salvando alterações antes do pull

---
+++ exec-enviar.sh
@@ -0,0 +1,11 @@
+# Adiciona todos os arquivos modificados
+git add .
+
+# Cria um commit com uma mensagem
+git commit -m "Salvando alterações antes do pull"
+
+# Atualiza a branch local com a versão remota, reorganizando os commits locais
+#git pull origin main --rebase
+
+# Agora sim! Envia as alterações locais para o repositório remoto
+git push origin main
